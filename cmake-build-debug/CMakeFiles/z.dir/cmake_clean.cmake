bf9385c41433c81ff695acaf5e19b612be7d607b
sadeqsoli <solisadeq@gmail.com>
2025-02-08 09:54:38 +0330

    making very first compiler code for Z language.

---
+++ cmake-build-debug/CMakeFiles/z.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/z.dir/src/main.cpp.o"
+  "CMakeFiles/z.dir/src/main.cpp.o.d"
+  "z"
+  "z.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/z.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
